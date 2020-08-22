0dad95ec630b5ba141dfa386c7bb0f2f41e5718d
Maksim-Vi <63706781+Maksim-Vi@users.noreply.github.com>
2020-08-22 16:31:24 +0300

    commit

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -4,7 +4,7 @@ import * as axios from 'axios'
 const InstenceAPI2 = axios.create({
     withCredentials: true,
     baseURL:'https://maksim-vi.github.io/', // обязательно такая запись иначе не работает baseURL
-    //baseURL:'http://localhost:5000/',
+    //baseURL:'http://localhost:3000/',
     headers:{
        'content-type':'application/json'
     }
