b5d9c65ee5caea8d3a7163dfe306e2c6b71170c8
Maksim-Vi <63706781+Maksim-Vi@users.noreply.github.com>
2020-08-22 14:07:31 +0300

    Updates

---
+++ precache-manifest.f1b6ddd819e616c013edf59b1a7b9938.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "282c4f894ccf87654556db7ea4f51190",
+    "url": "/Eco-Store/index.html"
+  },
+  {
+    "revision": "3bd67795f50c71529cef",
+    "url": "/Eco-Store/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "55dfac1dfcc94e1db3c4",
+    "url": "/Eco-Store/static/css/main.4850acab.chunk.css"
+  },
+  {
+    "revision": "3bd67795f50c71529cef",
+    "url": "/Eco-Store/static/js/2.310355b8.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/Eco-Store/static/js/2.310355b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55dfac1dfcc94e1db3c4",
+    "url": "/Eco-Store/static/js/main.a2288747.chunk.js"
+  },
+  {
+    "revision": "50326e8794d6785604bc",
+    "url": "/Eco-Store/static/js/runtime-main.079d0c2f.js"
+  },
+  {
+    "revision": "a51606ffb28407f0dcc6d4208c48171f",
+    "url": "/Eco-Store/static/media/ContactsBG.a51606ff.png"
+  },
+  {
+    "revision": "d2d4f4d3e43cfcb0c60aecfe5e2b5fa7",
+    "url": "/Eco-Store/static/media/delivery_transportation.d2d4f4d3.png"
+  },
+  {
+    "revision": "23ecb4ce5d6abb5746f2460313d7cc7f",
+    "url": "/Eco-Store/static/media/headerBG.23ecb4ce.png"
+  },
+  {
+    "revision": "9e32f2ab3e9de147a4633e07bc53f5b0",
+    "url": "/Eco-Store/static/media/korzina.9e32f2ab.png"
+  },
+  {
+    "revision": "9210b2451a4f19b0b8aef9ed85cc2e74",
+    "url": "/Eco-Store/static/media/map.9210b245.png"
+  },
+  {
+    "revision": "5a2892451f90cc942fb707bd6ce9c780",
+    "url": "/Eco-Store/static/media/naloshka new.5a289245.png"
+  },
+  {
+    "revision": "9edf309052e59a7f0c60697bcdbbc7ea",
+    "url": "/Eco-Store/static/media/pay.9edf3090.png"
+  },
+  {
+    "revision": "df20aa754563aa945ece6d4db9cdafce",
+    "url": "/Eco-Store/static/media/popularBG.df20aa75.png"
+  },
+  {
+    "revision": "65ebb302e2bd7306a2fdc51eec5ddaf0",
+    "url": "/Eco-Store/static/media/preloader.65ebb302.gif"
+  },
+  {
+    "revision": "7beb5518e3efc286f9542300e4a5ca15",
+    "url": "/Eco-Store/static/media/telegram.7beb5518.png"
+  },
+  {
+    "revision": "432140243b5a3e4f0d3fb7edd53548f0",
+    "url": "/Eco-Store/static/media/text.43214024.png"
+  },
+  {
+    "revision": "b0513a672c3ff2e640f16226e4d343df",
+    "url": "/Eco-Store/static/media/viber.b0513a67.png"
+  }
+]);
